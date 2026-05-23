b3636bc7ab1f64c20b3b687092ffc06c8c5913b7
el211 <saireelias@gmail.com>
2026-05-23 14:41:21 +0200

    complete minecraft chat bridge

--- handlers/chat_bridge.go
+++ handlers/chat_bridge.go
@@ -390,3 +390,9 @@ func b64dec(s string) string {
 
 func escapeJSON(s string) string {
 	s = strings.ReplaceAll(s, `\`, `\\`)
+	s = strings.ReplaceAll(s, `"`, `\"`)
+	s = strings.ReplaceAll(s, "\n", `\n`)
+	s = strings.ReplaceAll(s, "\r", `\r`)
+	s = strings.ReplaceAll(s, "\t", `\t`)
+	return s
+}
